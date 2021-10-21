054cc17e3dda4b071058113ab7e92ed30d1cdf5e
Mateusz Kopij <sagasu@gmail.com>
2021-10-21 14:57:49 +0100

    Supporting videos when they are retruned by API.

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # NASA-apod
 
 Simple picture of the day from [NASA API](https://api.nasa.gov/). Make sure you subscribe to api and generate a key for a configuration.
+
 # Config
-make sure to create `.env` file in this same folder as package.json. inside it create variables and set the NASA key.
+make sure to create `.env` file in this same folder as package.json. inside it create variables and set the NASA key.  
+`REACT_APP_NASA_KEY=""`  
 
 # Running
 `npm start`
--- src/components/NasaPhoto.js
+++ src/components/NasaPhoto.js
@@ -1,8 +1,10 @@
 import React, {useEffect, useState} from 'react'
 
+const apikey = process.env.REACT_APP_NASA_KEY;
+
 export const NasaPhoto = () => {
     const [photoData, setPhotoData] = useState(null);
-    const apikey = '';
+    
 
     useEffect(() => {
         const fetchPhoto = async () => {
@@ -17,12 +19,29 @@ export const NasaPhoto = () => {
 
     if(!photoData) return <div>Loading...</div>
 
+    let {title, url, date, explanation, media_type} = photoData;
     return (
         <div>
-            <img 
-                src={photoData.url}
-                alt={photoData.title}
-            />
+            {media_type === "image" ? (<img 
+                src={url}
+                alt={title}
+            />) : (
+                <iframe 
+                    title="space-video"
+                    src={url}
+                    frameBorder="0"
+                    gesture="media"
+                    allow="encrypted-media"
+                    allowFullScreen
+                    className="photo"
+                />
+            )}
+            
+            <div>
+                <h1>{title}</h1>
+                <p>{date}</p>
+                <p>{explanation}</p>
+            </div>
         </div>
     )
 }
