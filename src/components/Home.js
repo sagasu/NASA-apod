330472ed351a6e32faefc204a2539cc15c921819
Mateusz Kopij <sagasu@gmail.com>
2021-10-21 14:38:09 +0100

    fetching photo of the day from NASA API

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
 
 export const Home = () => {
     return (
         <div>
-            
+            <Link to="/nasaphoto">See into the stars!</Link>            
         </div>
     )
 }
--- src/components/NasaPhoto.js
+++ src/components/NasaPhoto.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 export const NasaPhoto = () => {
+    const [photoData, setPhotoData] = useState(null);
+    const apikey = '';
+
+    useEffect(() => {
+        const fetchPhoto = async () => {
+            const res = await fetch(
+                `https://api.nasa.gov/planetary/apod?api_key=${apikey}`
+            );
+            const data = await res.json();
+            setPhotoData(data);
+        };
+        fetchPhoto();
+    }, []);
+
+    if(!photoData) return <div>Loading...</div>
+
     return (
         <div>
-            
+            <img 
+                src={photoData.url}
+                alt={photoData.title}
+            />
         </div>
     )
 }
