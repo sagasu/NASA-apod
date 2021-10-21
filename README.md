3d40db691b7517b08aba71ff8b7e4036126222db
Mateusz Kopij <sagasu@gmail.com>
2021-10-21 14:07:17 +0100

    basic project structure

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # NASA-apod
 
+Simple picture of the day from [NASA API](https://api.nasa.gov/). Make sure you subscribe to api and generate a key for a configuration.
+# Config
+make sure to create `.env` file in this same folder as package.json. inside it create variables and set the NASA key.
 
-### `npm start`
+# Running
+`npm start`
+
--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {BrowserRouter as Router, Route} from 'react-router-dom'; 
+import {Home} from "./components/Home";
+import {NasaPhoto} from "./components/NasaPhoto";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Route component={Home} path="/" exact />
+        <Route component={NasaPhoto} path="/nasaphoto" />
+      </div>
+    </Router>
   );
 }
 
